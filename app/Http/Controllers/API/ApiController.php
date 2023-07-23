6406c195c2fc7cfe09163ccc7b06c1c557aa9a74
Alex Noppenberger <noppenbe@gmx.de>
2023-07-23 18:53:20 +0200

    Update ApiController.php

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -35,10 +35,10 @@ class ApiController extends BaseController
         // Check the success status and send response back
         $success  = !(isset($powercloud_response['success'])) || $powercloud_response['success'] != true ? false : true; 
         if ($success){
-            return $this->sendResponse(new Ftpapi($powercloud_response), 'Order created.',200);
+            return $this->sendResponse(new Ftpapi(json_decode($powercloud_response,1)), 'Order created.',200);
         }
         else{
-             return $this->sendError('Refused from Powercloud', new Ftpapi($powercloud_response), 400);
+             return $this->sendError('Refused from Powercloud', new Ftpapi(json_decode($powercloud_response,1)), 400);
         }
         
     }
